195204f2d4b6d76b390758b9300a5ddca8752a10
Dkingofcode <doladepo128@gmail.com>
2023-03-28 14:36:26 +0100

    Added readmore button

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -9,10 +9,12 @@ const Tour = ({ id, image, info, name, price, removeTours }) => {
      <article className='tour'>
        <img className='tour-img' src={image} alt={name} />
         <div className='tour-info'>
-         <h3>{name}</h3>
-         <h4>{info}</h4>
          <p>{price}</p>
+         <h3>{name}</h3>
+         { readMore ? <h4>{info}</h4> : `<h4>${(info.substring(0, 180))}</h4>`}
+         <button type='button' className='btn'>{readMore ? 'show-less' : 'show-more'}</button>
         </div>
+
         <button onClick={() => removeTours(id)}>Not interested</button>
       </article>   
     </div>
